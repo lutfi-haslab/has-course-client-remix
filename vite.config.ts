2440393c30c77177276e95a28edc9176a707b4f0
HY4-MAC-002 <hy4-mac-002@HY4-MAC-002.local>
2024-09-25 10:14:19 +0700

    add sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [
-    remix({
+
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [remix({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
-    }),
-    tsconfigPaths(),
-  ],
-});
+    }), tsconfigPaths(), sentryVitePlugin({
+      debug: true,
+      org: "haslab",
+      project: "has-course-client",
+      url: "https://sentry.io/",
+      authToken: process.env.SENTRY_AUTH_TOKEN, // Menggunakan process.env untuk menghindari error
+    })],
+    build: {
+      sourcemap: true
+    }
+  }
+})
